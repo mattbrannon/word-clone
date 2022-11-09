ac9652c6e8dacf317a8fc8621265c2938d802c94
Matt Brannon <mattbrannon.dev@gmail.com>
2022-11-09 07:09:33 -0600

    GuessInput

---
+++ src/components/GuessInput/GuessInput.js
@@ -0,0 +1,31 @@
+import React from 'react';
+
+function GuessInput() {
+  const [value, setValue] = React.useState('');
+
+  const handleSubmitGuess = (e) => {
+    e.preventDefault();
+    if (value.length < 5) {
+      return window.alert('Please enter a 5 character word');
+    }
+    console.log(value);
+    setValue('');
+  };
+
+  return (
+    <form onSubmit={handleSubmitGuess} className="guess-input-wrapper">
+      <label htmlFor="guess-input">Enter guess:</label>
+      <input
+        id="guess-input"
+        type="text"
+        value={value}
+        onChange={(e) => setValue(e.target.value.toUpperCase())}
+        minLength={5}
+        maxLength={5}
+        required
+      />
+    </form>
+  );
+}
+
+export default GuessInput;
